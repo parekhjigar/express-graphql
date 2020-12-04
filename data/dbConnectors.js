b073bcd0e13354e7fe55abdbafc015a1bb1697d9
parekhjigar <hello@parekhjigar.com>
2020-12-04 13:20:57 +1100

    Updated dbConnection

--- data/dbConnectors.js
+++ data/dbConnectors.js
@@ -48,4 +48,14 @@ const Aliens = sequelize.define("aliens", {
   planet: { type: Sequelize.STRING },
 });
 
+Aliens.sync({ force: true }).then(() => {
+  _.times(10, (i) => {
+    Aliens.create({
+      firstName: casual.first_name,
+      lastName: casual.last_name,
+      planet: casual.word,
+    });
+  });
+});
+
 export { Friends, Aliens };
